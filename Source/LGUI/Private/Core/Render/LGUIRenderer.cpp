9bdd7fa58903c9c2b15dffbce2e633ab96948c0a
lf2007hf <707908214@qq.com>
2020-01-20 12:37:51 +0800

    modify copyright to 2019-2020

--- Source/LGUI/Private/Core/Render/LGUIRenderer.cpp
+++ Source/LGUI/Private/Core/Render/LGUIRenderer.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 LexLiu. All Rights Reserved.
+﻿// Copyright 2019-2020 LexLiu. All Rights Reserved.
 
 #include "Core/Render/LGUIRenderer.h"
 #include "Core/Render/LGUIShaders.h"
--- Source/LGUIEditor/Private/DetailCustomization/LGUIFontDataCustomization.cpp
+++ Source/LGUIEditor/Private/DetailCustomization/LGUIFontDataCustomization.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 LexLiu. All Rights Reserved.
+﻿// Copyright 2019-2020 LexLiu. All Rights Reserved.
 
 #include "DetailCustomization/LGUIFontDataCustomization.h"
 #include "Misc/FileHelper.h"
