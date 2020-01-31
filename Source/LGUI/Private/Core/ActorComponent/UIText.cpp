e34727e013c46ed313bb092f5f7e4af79ecd3b02
lf2007hf <707908214@qq.com>
2020-01-31 13:36:12 +0800

    Reconstruct UIText's geometry, now the code is more clean

--- Source/LGUI/Private/Core/ActorComponent/UIText.cpp
+++ Source/LGUI/Private/Core/ActorComponent/UIText.cpp
@@ -5,6 +5,7 @@
 #include "Core/UIGeometry.h"
 #include "Core/ActorComponent/LGUICanvas.h"
 #include "Materials/MaterialInterface.h"
+#include "Core/LGUIFontData.h"
 
 
 #if WITH_EDITORONLY_DATA
@@ -32,14 +33,6 @@ void UUIText::ApplyFontTextureScaleUp()
 		}
 		MarkUVDirty();
 	}
-	for (int i = 0, count = cachedTextGeometryList.Num(); i < count; i++)
-	{
-		auto& textGeo = cachedTextGeometryList[i];
-		textGeo.uv0 *= 0.5f;
-		textGeo.uv1 *= 0.5f;
-		textGeo.uv2 *= 0.5f;
-		textGeo.uv3 *= 0.5f;
-	}
 	geometry->texture = font->texture;
 	if (CheckRenderCanvas())
 	{
@@ -52,7 +45,6 @@ void UUIText::ApplyFontTextureChange()
 	if (IsValid(font))
 	{
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 		MarkTriangleDirty();
 		MarkTextureDirty();
 		geometry->texture = font->texture;
@@ -68,7 +60,6 @@ void UUIText::ApplyRecreateText()
 	if (IsValid(font))
 	{
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 		MarkVertexPositionDirty();
 	}
 }
@@ -171,7 +162,7 @@ void UUIText::OnCreateGeometry()
 	geometry->isFontTexture = true;
 	float width = widget.width;
 	float height = widget.height;
-	UIGeometry::FromUIText(text, visibleCharCount, width, height, widget.pivot, GetFinalColor(), space, geometry, size, (int8)hAlign, (int8)vAlign, (uint8)overflowType, adjustWidth, adjustHeight, (uint8)fontStyle, textRealSize, RenderCanvas->GetDynamicPixelsPerUnit(), cachedTextPropertyList, cachedTextGeometryList, RenderCanvas->GetRequireNormal(), RenderCanvas->GetRequireTangent(), RenderCanvas->GetRequireUV1());
+	UIGeometry::FromUIText(text, visibleCharCount, width, height, widget.pivot, GetFinalColor(), space, geometry, size, hAlign, vAlign, overflowType, adjustWidth, adjustHeight, fontStyle, textRealSize, RenderCanvas, cachedTextPropertyList, font);
 	SetWidth(width);
 	SetHeight(height);
 }
@@ -185,7 +176,7 @@ void UUIText::OnUpdateGeometry(bool InVertexPositionChanged, bool InVertexUVChan
 	{
 		float width = widget.width;
 		float height = widget.height;
-		UIGeometry::UpdateUITextVertexOrUV(text, width, height, widget.pivot, space, geometry, size, (uint8)hAlign, (uint8)vAlign, (uint8)overflowType, adjustWidth, adjustHeight, (uint8)fontStyle, textRealSize, RenderCanvas->GetDynamicPixelsPerUnit(), true, InVertexUVChanged, cachedTextPropertyList, cachedTextGeometryList);
+		UIGeometry::UpdateUITextVertexOrUV(text, visibleCharCount, width, height, widget.pivot, space, geometry, size, hAlign, vAlign, overflowType, adjustWidth, adjustHeight, fontStyle, textRealSize, RenderCanvas, cachedTextPropertyList, font);
 		SetWidth(width);
 		SetHeight(height);
 	}
@@ -213,7 +204,7 @@ void UUIText::EditorForceUpdateImmediately()
 			font->AddUIText(this);
 		}
 	}
-	cachedTextGeometryList.Reset();
+	cachedTextPropertyList.Reset();
 }
 void UUIText::OnPreChangeFontProperty()
 {
@@ -246,7 +237,7 @@ FVector2D UUIText::GetRealSize()
 		{
 			float width = widget.width;
 			float height = widget.height;
-			UIGeometry::UpdateUITextVertexOrUV(text, width, height, widget.pivot, space, geometry, size, (uint8)hAlign, (uint8)vAlign, (uint8)overflowType, adjustWidth, adjustHeight, (uint8)fontStyle, textRealSize, RenderCanvas->GetDynamicPixelsPerUnit(), false, false, cachedTextPropertyList, cachedTextGeometryList);
+			UIGeometry::UpdateUITextVertexOrUV(text, visibleCharCount, width, height, widget.pivot, space, geometry, size, hAlign, vAlign, overflowType, adjustWidth, adjustHeight, fontStyle, textRealSize, RenderCanvas, cachedTextPropertyList, font);
 			SetWidth(width);
 			SetHeight(height);
 		}
@@ -270,7 +261,6 @@ void UUIText::SetFont(ULGUIFontData* newFont) {
 		}
 		font = newFont;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 		MarkTextureDirty();
 		//add to new
 		if (IsValid(font))
@@ -286,7 +276,6 @@ void UUIText::SetText(const FString& newText) {
 		text = newText;
 		text.ReplaceInline(TEXT("\r\n"), TEXT("\n"));
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 
 		int newVisibleCharCount = VisibleCharCountInString(text);
 		if (newVisibleCharCount != visibleCharCount)//visible char count change
@@ -294,7 +283,7 @@ void UUIText::SetText(const FString& newText) {
 			MarkTriangleDirty();
 			visibleCharCount = newVisibleCharCount;
 		}
-		else//visible char count not change, just makr update vertex and uv
+		else//visible char count not change, just mark update vertex and uv
 		{
 			MarkVertexPositionDirty();
 			MarkUVDirty();
@@ -308,7 +297,6 @@ void UUIText::SetFontSize(float newSize) {
 		MarkVertexPositionDirty();
 		size = newSize;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetFontSpace(FVector2D newSpace) {
@@ -317,7 +305,6 @@ void UUIText::SetFontSpace(FVector2D newSpace) {
 		MarkVertexPositionDirty();
 		space = newSpace;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetParagraphHorizontalAlignment(UITextParagraphHorizontalAlign newHAlign) {
@@ -326,7 +313,6 @@ void UUIText::SetParagraphHorizontalAlignment(UITextParagraphHorizontalAlign new
 		MarkVertexPositionDirty();
 		hAlign = newHAlign;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetParagraphVerticalAlignment(UITextParagraphVerticalAlign newVAlign) {
@@ -335,7 +321,6 @@ void UUIText::SetParagraphVerticalAlignment(UITextParagraphVerticalAlign newVAli
 		MarkVertexPositionDirty();
 		vAlign = newVAlign;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetOverflowType(UITextOverflowType newOverflowType) {
@@ -347,7 +332,6 @@ void UUIText::SetOverflowType(UITextOverflowType newOverflowType) {
 			MarkVertexPositionDirty();
 		overflowType = newOverflowType;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetAdjustWidth(bool newAdjustWidth) {
@@ -356,7 +340,6 @@ void UUIText::SetAdjustWidth(bool newAdjustWidth) {
 		adjustWidth = newAdjustWidth;
 		MarkVertexPositionDirty();
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetAdjustHeight(bool newAdjustHeight) {
@@ -365,7 +348,6 @@ void UUIText::SetAdjustHeight(bool newAdjustHeight) {
 		adjustHeight = newAdjustHeight;
 		MarkVertexPositionDirty();
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::SetFontStyle(UITextFontStyle newFontStyle) {
@@ -382,80 +364,15 @@ void UUIText::SetFontStyle(UITextFontStyle newFontStyle) {
 		}
 		fontStyle = newFontStyle;
 		cachedTextPropertyList.Reset();
-		cachedTextGeometryList.Reset();
 	}
 }
 void UUIText::CacheTextGeometry()
 {
-	if (!IsValid(RenderCanvas))return;//render canvas not valid
-	if (!IsValid(font))return;//font not valid
-	if (cachedTextGeometryList.Num() != 0)return;//have cache data
-	float charFixOffset = size * (font->fixedVerticalOffset - 0.25f);//some font may not render at vertical center, use this to mofidy it. 0.25 * size is tested value for most fonts
-	float halfFontSize = size * 0.5f;
-
-	bool pixelPerfect = RenderCanvas->GetPixelPerfect();
-	bool dynamicPixelsPerUnitIsNot1 = RenderCanvas->GetDynamicPixelsPerUnit() != 1;//use dynamicPixelsPerUnit or not
-	float calcFontSize = size;
-	if (pixelPerfect)
-	{
-		calcFontSize = calcFontSize / RenderCanvas->GetRootCanvas()->GetViewportUIScale();
-		calcFontSize = calcFontSize > 200.0f ? 200.0f : calcFontSize;//limit font size to 200. too large font size will result in large texture
-	}
-	else if(dynamicPixelsPerUnitIsNot1)
-	{
-		calcFontSize = calcFontSize * RenderCanvas->GetDynamicPixelsPerUnit();
-		calcFontSize = calcFontSize > 200.0f ? 200.0f : calcFontSize;//limit font size to 200. too large font size will result in large texture
-	}
-
-	bool bold = fontStyle == UITextFontStyle::Bold || fontStyle == UITextFontStyle::BoldAndItalic;
-	bool italic = fontStyle == UITextFontStyle::Italic || fontStyle == UITextFontStyle::BoldAndItalic;
 	if (visibleCharCount == -1)visibleCharCount = VisibleCharCountInString(text);
-	cachedTextGeometryList.Reset(visibleCharCount);
-	for (int i = 0, count = text.Len(); i < count; i++)
-	{
-		TCHAR charCode = text[i];
-		if (!IsVisibleChar(charCode))continue;//skip invisible chars
-		FUITextCharGeometry charGeometry;
-		if (charCode == ' ')
-		{
-			charGeometry.xadvance = halfFontSize;
-			charGeometry.geoWidth = charGeometry.geoHeight = 0;
-			charGeometry.xoffset = charGeometry.yoffset = 0;
-			charGeometry.uv0 = charGeometry.uv1 = charGeometry.uv2 = charGeometry.uv3 = FVector2D(1, 1);
-		}
-		else if (charCode == '\t')
-		{
-			charGeometry.xadvance = size + size;
-			charGeometry.geoWidth = charGeometry.geoHeight = 0;
-			charGeometry.xoffset = charGeometry.yoffset = 0;
-			charGeometry.uv0 = charGeometry.uv1 = charGeometry.uv2 = charGeometry.uv3 = FVector2D(1, 1);
-		}
-		else
-		{
-			auto charData = font->GetCharData(charCode, size, bold, italic);
-			charGeometry.geoWidth = charData->width;
-			charGeometry.geoHeight = charData->height;
-			charGeometry.xadvance = charData->xadvance;
-			charGeometry.xoffset = charData->xoffset;
-			charGeometry.yoffset = charData->yoffset + charFixOffset;
-
-			auto overrideCharData = charData;
-			if (pixelPerfect || dynamicPixelsPerUnitIsNot1)
-			{
-				overrideCharData = font->GetCharData(charCode, (uint16)calcFontSize, bold, italic);
-			}
-
-			charGeometry.uv0 = overrideCharData->GetUV0();
-			charGeometry.uv1 = overrideCharData->GetUV1();
-			charGeometry.uv2 = overrideCharData->GetUV2();
-			charGeometry.uv3 = overrideCharData->GetUV3();
-		}
-		cachedTextGeometryList.Add(charGeometry);
-	}
 }
 void UUIText::MarkAllDirtyRecursive()
 {
-	cachedTextGeometryList.Reset();
+	cachedTextPropertyList.Reset();
 	Super::MarkAllDirtyRecursive();
 }
 FVector2D UUIText::GetCharSize(TCHAR character)
@@ -485,7 +402,7 @@ int UUIText::VisibleCharCountInString(const FString& srcStr)
 	for (int i = 0; i < count; i++)
 	{
 		auto charIndexItem = srcStr[i];
-		if (IsVisibleChar(charIndexItem) == false)// 10 - /n, 13 - /r, 32 - space, /t - tab
+		if (IsVisibleChar(charIndexItem) == false)
 		{
 			continue;
 		}
@@ -505,7 +422,7 @@ void UUIText::CheckCachedTextPropertyList()
 		CacheTextGeometry();
 		float width = widget.width;
 		float height = widget.height;
-		UIGeometry::UpdateUITextVertexOrUV(text, width, height, widget.pivot, space, geometry, size, (uint8)hAlign, (uint8)vAlign, (uint8)overflowType, adjustWidth, adjustHeight, (uint8)fontStyle, textRealSize, RenderCanvas->GetDynamicPixelsPerUnit(), false, false, cachedTextPropertyList, cachedTextGeometryList);
+		UIGeometry::UpdateUITextVertexOrUV(text, visibleCharCount, width, height, widget.pivot, space, geometry, size, hAlign, vAlign, overflowType, adjustWidth, adjustHeight, fontStyle, textRealSize, RenderCanvas, cachedTextPropertyList, font);
 		SetWidth(width);
 		SetHeight(height);
 	}
