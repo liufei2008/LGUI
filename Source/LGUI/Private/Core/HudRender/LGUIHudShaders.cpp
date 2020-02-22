1c6071f507007d370638d0826d88678a1e9387fc
lf2007hf <707908214@qq.com>
2020-02-22 18:50:01 +0800

    Split out post process shader from LGUIHudShaders
    (cherry picked from commit a68e70e5f1297cc2523074db7a6ccd5499038e8f)

--- Source/LGUI/Private/Core/HudRender/LGUIHudShaders.cpp
+++ Source/LGUI/Private/Core/HudRender/LGUIHudShaders.cpp
@@ -100,10 +100,3 @@ bool FLGUIHudRenderPS::Serialize(FArchive& Ar)
 	bool bShaderHasOutdatedParameters = FMaterialShader::Serialize(Ar);
 	return bShaderHasOutdatedParameters;
 }
-
-
-IMPLEMENT_SHADER_TYPE(, FLGUISimplePostProcessVS, TEXT("/Plugin/LGUI/Private/LGUIPostProcessVertexShader.usf"), TEXT("SimplePostProcessVS"), SF_Vertex)
-IMPLEMENT_SHADER_TYPE(, FLGUIMeshPostProcessVS, TEXT("/Plugin/LGUI/Private/LGUIPostProcessVertexShader.usf"), TEXT("MeshPostProcessVS"), SF_Vertex)
-IMPLEMENT_SHADER_TYPE(, FLGUIPostProcessGaussianBlurPS, TEXT("/Plugin/LGUI/Private/LGUIPostProcessGaussianBlur.usf"), TEXT("GaussianBlurPS"), SF_Pixel)
-IMPLEMENT_SHADER_TYPE(, FLGUISimpleCopyTargetPS, TEXT("/Plugin/LGUI/Private/LGUICopyTargetShader.usf"), TEXT("SimpleCopyTargetPS"), SF_Pixel)
-IMPLEMENT_SHADER_TYPE(, FLGUIMeshCopyTargetPS, TEXT("/Plugin/LGUI/Private/LGUICopyTargetShader.usf"), TEXT("MeshCopyTargetPS"), SF_Pixel)
