3496fe7f503b98a156e496eccd4eb6e85b2fb611
lf2007hf <707908214@qq.com>
2020-06-14 11:58:32 +0800

    fix a build warning of bind none const function
    (cherry picked from commit 7a8fe2fc84f6f0936f464c6f5be906aeb042156d)

--- Source/LGUIEditor/Private/Widget/ComponentTransformDetails.cpp
+++ Source/LGUIEditor/Private/Widget/ComponentTransformDetails.cpp
@@ -196,7 +196,7 @@ void FComponentTransformDetails::OnPaste( ETransformField::Type TransformField )
 	}
 }
 
-FUIAction FComponentTransformDetails::CreateCopyAction( ETransformField::Type TransformField ) const
+FUIAction FComponentTransformDetails::CreateCopyAction( ETransformField::Type TransformField ) 
 {
 	return
 		FUIAction
@@ -206,7 +206,7 @@ FUIAction FComponentTransformDetails::CreateCopyAction( ETransformField::Type Tr
 		);
 }
 
-FUIAction FComponentTransformDetails::CreatePasteAction( ETransformField::Type TransformField ) const
+FUIAction FComponentTransformDetails::CreatePasteAction( ETransformField::Type TransformField ) 
 {
 	return 
 		 FUIAction( FExecuteAction::CreateSP(this, &FComponentTransformDetails::OnPaste, TransformField ) );
