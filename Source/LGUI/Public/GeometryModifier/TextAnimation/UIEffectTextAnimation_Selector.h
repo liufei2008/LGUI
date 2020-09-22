89e695ab3b78eeffd9a928e604cea1070cafa8aa
lf2007hf <707908214@qq.com>
2020-09-22 21:06:29 +0800

    UIEffectTextAnimation:easeType和easeCurve都放到property里面. 完善LGUIHeaders.
    (cherry picked from commit a316298b2c056b4b43baa41f3cc96efdf7021714)

--- Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Selector.h
+++ Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Selector.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "../UIEffectTextAnimation.h"
-#include "LTweener.h"
 #include "UIEffectTextAnimation_Selector.generated.h"
 
 class UCurveFloat;
@@ -15,21 +14,9 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Property", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 		float range = 0.1f;
 	UPROPERTY(EditAnywhere, Category = "Property", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-		float offset = 0.0f;
+		float offset = 0.5f;
 	UPROPERTY(EditAnywhere, Category = "Property")
 		bool flipDirection = false;
-	UPROPERTY(EditAnywhere, Category = "Property")
-		LTweenEase easeLerp = LTweenEase::InOutSine;
-	//only valid if easeLerp = CurveFloat
-	UPROPERTY(EditAnywhere, Category = "Property")
-		UCurveFloat* curveFloat;
-
-	FLTweenFunction easeFunc;
-	float CurveFloat(float c, float b, float t, float d);
-	void CheckAndInitSelector();
-#if WITH_EDITOR
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
-#endif
 public:
 	virtual bool Select(class UUIText* InUIText, TArray<FUIEffectTextAnimation_SelectResult>& OutSelection)override;
 
@@ -39,21 +26,13 @@ public:
 		float GetOffset()const { return offset; }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		bool GetFlipDirection()const { return flipDirection; }
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		LTweenEase GetEaseLerp()const { return easeLerp; }
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		UCurveFloat* GetCurveFloat()const { return curveFloat; }
-
+	
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetRange(float value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetOffset(float value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetFlipDirection(bool value);
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		void SetEaseLerp(LTweenEase value);
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		void SetCurveFloat(UCurveFloat* value);
 };
 
 UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "Random"))
@@ -61,20 +40,27 @@ class LGUI_API UUIEffectTextAnimation_RandomSelector : public UUIEffectTextAnima
 {
 	GENERATED_BODY()
 private:
+	//random seed
 	UPROPERTY(EditAnywhere, Category = "Property")
 		int seed = 0;
 	UPROPERTY(EditAnywhere, Category = "Property", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-		float strength = 1.0f;
+		float offset = 0.5f;
+	UPROPERTY(EditAnywhere, Category = "Property")
+		bool flipDirection = false;
 public:
 	virtual bool Select(class UUIText* InUIText, TArray<FUIEffectTextAnimation_SelectResult>& OutSelection)override;
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		int GetSeed()const { return seed; }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		float GetStrength()const { return strength; }
+		float GetOffset()const { return offset; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		bool GetFlipDirection()const { return flipDirection; }
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetSeed(int value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		void SetStrength(float value);
+		void SetOffset(float value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetFlipDirection(bool value);
 };
