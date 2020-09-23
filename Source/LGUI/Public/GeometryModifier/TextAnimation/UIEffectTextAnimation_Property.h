1d8376617f7733d26305b6c4a543e75dfd3f1847
lf2007hf <707908214@qq.com>
2020-09-23 10:05:47 +0800

    添加PositionRandom/RotationRandom/ScaleRandom/ColorRandom
    (cherry picked from commit 777c92381865059c12eec6f13f21861e7f1daa77)

--- Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Property.h
+++ Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Property.h
@@ -23,6 +23,38 @@ public:
 		void SetPosition(FVector value);
 };
 
+UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "PositionRandom"))
+class LGUI_API UUIEffectTextAnimation_PositionRandomProperty : public UUIEffectTextAnimation_Property
+{
+	GENERATED_BODY()
+private:
+	//random seed
+	UPROPERTY(EditAnywhere, Category = "Property")
+		int seed = 0;
+	//random min
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FVector min = FVector(0, 0, 0);
+	//random max
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FVector max = FVector(0, 10, 0);
+public:
+	virtual void ApplyProperty(class UUIText* InUIText, const TArray<FUIEffectTextAnimation_SelectResult>& InSelection, TSharedPtr<UIGeometry> OutGeometry) override;
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		int GetSeed()const { return seed; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FVector GetMin()const { return min; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FVector GetMax()const { return max; }
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetSeed(int value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMin(FVector value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMax(FVector value);
+};
+
 UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "Rotation"))
 class LGUI_API UUIEffectTextAnimation_RotationProperty : public UUIEffectTextAnimation_Property
 {
@@ -40,6 +72,38 @@ public:
 		void SetRotator(FRotator value);
 };
 
+UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "RotationRandom"))
+class LGUI_API UUIEffectTextAnimation_RotationRandomProperty : public UUIEffectTextAnimation_Property
+{
+	GENERATED_BODY()
+private:
+	//random seed
+	UPROPERTY(EditAnywhere, Category = "Property")
+		int seed = 0;
+	//random min
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FRotator min = FRotator(0, 0, 0);
+	//random max
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FRotator max = FRotator(0, 90, 0);
+public:
+	virtual void ApplyProperty(class UUIText* InUIText, const TArray<FUIEffectTextAnimation_SelectResult>& InSelection, TSharedPtr<UIGeometry> OutGeometry) override;
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		int GetSeed()const { return seed; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FRotator GetMin()const { return min; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FRotator GetMax()const { return max; }
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetSeed(int value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMin(FRotator value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMax(FRotator value);
+};
+
 UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "Scale"))
 class LGUI_API UUIEffectTextAnimation_ScaleProperty : public UUIEffectTextAnimation_Property
 {
@@ -55,6 +119,38 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetScale(FVector value);
+};
+
+UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "ScaleRandom"))
+class LGUI_API UUIEffectTextAnimation_ScaleRandomProperty : public UUIEffectTextAnimation_Property
+{
+	GENERATED_BODY()
+private:
+	//random seed
+	UPROPERTY(EditAnywhere, Category = "Property")
+		int seed = 0;
+	//random min
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FVector min = FVector(1, 1, 1);
+	//random max
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FVector max = FVector(2, 2, 2);
+public:
+	virtual void ApplyProperty(class UUIText* InUIText, const TArray<FUIEffectTextAnimation_SelectResult>& InSelection, TSharedPtr<UIGeometry> OutGeometry) override;
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		int GetSeed()const { return seed; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FVector GetMin()const { return min; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FVector GetMax()const { return max; }
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetSeed(int value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMin(FVector value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMax(FVector value);
 };
 
 UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "Alpha"))
@@ -80,7 +176,7 @@ class LGUI_API UUIEffectTextAnimation_ColorProperty : public UUIEffectTextAnimat
 	GENERATED_BODY()
 private:
 	UPROPERTY(EditAnywhere, Category = "Property")
-		FColor color = FColor::White;
+		FColor color = FColor::Green;
 public:
 	virtual void ApplyProperty(class UUIText* InUIText, const TArray<FUIEffectTextAnimation_SelectResult>& InSelection, TSharedPtr<UIGeometry> OutGeometry) override;
 
@@ -89,4 +185,36 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetColor(FColor value);
+};
+
+UCLASS(ClassGroup = (LGUI), BlueprintType, meta = (DisplayName = "ColorRandom"))
+class LGUI_API UUIEffectTextAnimation_ColorRandomProperty : public UUIEffectTextAnimation_Property
+{
+	GENERATED_BODY()
+private:
+	//random seed
+	UPROPERTY(EditAnywhere, Category = "Property")
+		int seed = 0;
+	//random min
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FColor min = FColor::Green;
+	//random max
+	UPROPERTY(EditAnywhere, Category = "Property")
+		FColor max = FColor::Red;
+public:
+	virtual void ApplyProperty(class UUIText* InUIText, const TArray<FUIEffectTextAnimation_SelectResult>& InSelection, TSharedPtr<UIGeometry> OutGeometry) override;
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		int GetSeed()const { return seed; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FColor GetMin()const { return min; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		FColor GetMax()const { return max; }
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetSeed(int value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMin(FColor value);
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetMax(FColor value);
 };
