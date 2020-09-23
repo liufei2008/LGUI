7598ea023ad726969abd6e0b22e92e37692ac4cf
lf2007hf <707908214@qq.com>
2020-09-23 08:47:15 +0800

    去掉RendomSelector的FlipDirection属性. RangeSelector完善FlipDirection.

--- Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Selector.h
+++ Source/LGUI/Public/GeometryModifier/TextAnimation/UIEffectTextAnimation_Selector.h
@@ -45,8 +45,6 @@ private:
 		int seed = 0;
 	UPROPERTY(EditAnywhere, Category = "Property", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 		float offset = 0.5f;
-	UPROPERTY(EditAnywhere, Category = "Property")
-		bool flipDirection = false;
 public:
 	virtual bool Select(class UUIText* InUIText, TArray<FUIEffectTextAnimation_SelectResult>& OutSelection)override;
 
@@ -54,13 +52,9 @@ public:
 		int GetSeed()const { return seed; }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		float GetOffset()const { return offset; }
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		bool GetFlipDirection()const { return flipDirection; }
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetSeed(int value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetOffset(float value);
-	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		void SetFlipDirection(bool value);
 };
