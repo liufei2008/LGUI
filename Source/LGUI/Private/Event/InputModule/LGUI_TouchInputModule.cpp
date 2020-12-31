b3003829c4cf1441be0828b1b5e3bc42beed3569
lf2007hf <707908214@qq.com>
2020-12-31 08:47:52 +0800

    修复：点击选择UI元素错误
    (cherry picked from commit 02784c24c8c0221a6e95293dbaccf240314d046e)
    (cherry picked from commit 47370c48d4b5783ac30a22a13616913b02bf341f)

--- Source/LGUI/Private/Event/InputModule/LGUI_TouchInputModule.cpp
+++ Source/LGUI/Private/Event/InputModule/LGUI_TouchInputModule.cpp
@@ -33,7 +33,7 @@ void ULGUI_TouchInputModule::InputScroll(const float& inAxisValue)
 	auto eventData = GetPointerEventData(0, true);
 	if (IsValid(eventData->enterComponent))
 	{
-		if (inAxisValue != eventData->scrollAxisValue)
+		if (inAxisValue != 0)
 		{
 			eventData->scrollAxisValue = inAxisValue;
 			if (CheckEventSystem())
