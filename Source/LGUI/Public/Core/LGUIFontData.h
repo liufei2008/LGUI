94e7e81e3793d5ba7d7f840598db33dbb24054bc
lf2007hf <707908214@qq.com>
2021-01-09 11:37:09 +0800

    修改一些注释格式

--- Source/LGUI/Public/Core/LGUIFontData.h
+++ Source/LGUI/Public/Core/LGUIFontData.h
@@ -2,8 +2,6 @@
 
 #pragma once
 
-#pragma warning(disable:4668)//error C4668: '__SUNPRO_C' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
-
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "RHI.h"
@@ -68,6 +66,10 @@ public:
 };
 
 class UUIText;
+
+/**
+ * font asset for UIText to render
+ */
 UCLASS(BlueprintType)
 class LGUI_API ULGUIFontData : public UObject
 {
@@ -76,29 +78,31 @@ public:
 	ULGUIFontData();
 	~ULGUIFontData();
 
-	//Font file path, absolute path or relative to ProjectDir
+	/** Font file path, absolute path or relative to ProjectDir */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		FString fontFilePath;
-	//Font file use relative path(relative to ProjectDir) or absolute path. After build your game, remember to copy your font file to target path, unless "useExternalFileOrEmbedInToUAsset" is false
+	/** Font file use relative path(relative to ProjectDir) or absolute path. After build your game, remember to copy your font file to target path, unless "useExternalFileOrEmbedInToUAsset" is false */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		bool useRelativeFilePath = true;
-	//When in build, use external file or embed into uasset. But in editor, will always load from fontFilePath.
+	/** When in build, use external file or embed into uasset. But in editor, will always load from fontFilePath. */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		bool useExternalFileOrEmbedInToUAsset = false;
-	//Some font text may not renderred at vertical center, use this to offset
+	/** Some font text may not renderred at vertical center, use this to offset */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		float fixedVerticalOffset = 0.0f;
-	//angle of italic style in degree
+	/** angle of italic style in degree */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		float italicAngle = 15.0f;
-	//bold size radio for bold style, large number create more bold effect
+	/** bold size radio for bold style, large number create more bold effect */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		float boldRatio = 0.015f;
-	//Packing tag of this font. If packingTag is not none, then LGUI will search UISprite's atlas packingTag, and pack font texture into sprite atlas's texture.
-	//This can be very useful to reduce drawcall.
+	/**
+	 * Packing tag of this font. If packingTag is not none, then LGUI will search UISprite's atlas packingTag, and pack font texture into sprite atlas's texture.
+	 * This can be very useful to reduce drawcall.
+	 */
 	UPROPERTY(EditAnywhere, Category = "LGUI")
 		FName packingTag;
-	//Texture of this font
+	/** Texture of this font */
 	UPROPERTY(VisibleAnywhere, Category = "LGUI")
 		UTexture2D* texture;
 
@@ -106,30 +110,36 @@ public:
 	void DeinitFreeType();
 	virtual void FinishDestroy()override;
 
-	const int32 SPACE_NEED_EXPEND = 1;//when draw a rectangle, need to expend 1 pixel to avoid too sharp edge
+	/** when draw a rectangle, need to expend 1 pixel to avoid too sharp edge */
+	const int32 SPACE_NEED_EXPEND = 1;
 	const int32 SPACE_NEED_EXPENDx2 = SPACE_NEED_EXPEND * 2;
-	const int32 SPACE_BETWEEN_GLYPH = SPACE_NEED_EXPEND + 1;//space between char in texture
+	/** space between char in texture */
+	const int32 SPACE_BETWEEN_GLYPH = SPACE_NEED_EXPEND + 1;
 	const int32 SPACE_BETWEEN_GLYPHx2 = SPACE_BETWEEN_GLYPH * 2;
 
 	FORCEINLINE void AddUIText(UUIText* InText);
 	FORCEINLINE void RemoveUIText(UUIText* InText);
 private:
-	//Collection of UIText which use this font to render.
+	/** Collection of UIText which use this font to render. */
 	UPROPERTY(VisibleAnywhere, Transient, Category = "LGUI")
 		TArray<TWeakObjectPtr<UUIText>> renderTextArray;
 
 	friend class FLGUIFontDataCustomization;
 	UPROPERTY()
 		TArray<uint8> fontBinaryArray;
+	/** temp array for storing data, because freetype need to load font to memory and keep alive */
 	UPROPERTY(Transient)
-		TArray<uint8> tempFontBinaryArray;//temp array for storing data, because freetype need to load font to memory and keep alive
+		TArray<uint8> tempFontBinaryArray;
 	TMap<FLGUIFontKeyData, FLGUICharData> charDataMap;
 	struct FLGUIAtlasData* packingAtlasData = nullptr;
 	FDelegateHandle packingAtlasTextureExpandDelegateHandle;
 
-	rbp::MaxRectsBinPack binPack;//for rect packing
-	int32 textureSize;//current texture size
-	float fullTextureSizeReciprocal;//1.0 / textureSize
+	/** for rect packing */
+	rbp::MaxRectsBinPack binPack;
+	/** current texture size */
+	int32 textureSize;
+	/** 1.0 / textureSize */
+	float fullTextureSizeReciprocal;
 
 	FLGUIFontKeyData cacheFontKey;
 	FLGUICharData cacheCharData;
@@ -139,9 +149,10 @@ private:
 	bool alreadyInitialized = false;
 	bool usePackingTag = false;
 	FLGUICharData* PushCharIntoFont(const TCHAR& charIndex, const uint16& charSize);
-	/*Insert rect into area, assign pixel if succeed
-	 return: if can fit in rect area return true, else false
-	*/
+	/**
+	 * Insert rect into area, assign pixel if succeed
+	 * return: if can fit in rect area return true, else false
+	 */
 	bool PackRectAndInsertChar(int32 InExtraSpace, const FT_GlyphSlot& InSlot, rbp::MaxRectsBinPack& InOutBinpack, UTexture2D* InTexture);
 	void UpdateTextureRegions(UTexture2D* Texture, int32 MipIndex, uint32 NumRegions, FUpdateTextureRegion2D* Regions, uint32 SrcPitch, uint32 SrcBpp, uint8* SrcData, bool bFreeData);
 	void UpdateFontTextureRegion(UTexture2D* Texture, FUpdateTextureRegion2D* Region, uint32 SrcPitch, uint32 SrcBpp, uint8* SrcData);
