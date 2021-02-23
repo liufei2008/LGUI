2f18a43a4806c4b9944d34e5486a80b1fad1edd6
lf2007hf <707908214@qq.com>
2021-02-23 20:30:00 +0800

    Add image sequence player, 2 new component: UISpriteSequencePlayer, UISpriteSheetTexturePlayer.

---
+++ Source/LGUI/Public/Extensions/LGUIImageSequencePlayer.h
@@ -0,0 +1,66 @@
+﻿// Copyright 2019-2021 LexLiu. All Rights Reserved.
+
+#pragma once
+#include "Components/ActorComponent.h"
+#include "LGUIImageSequencePlayer.generated.h"
+
+UCLASS(Abstract)
+class LGUI_API ULGUIImageSequencePlayer : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	ULGUIImageSequencePlayer();
+protected:
+	/** fps: Frame per second */
+	UPROPERTY(EditAnywhere, Category = "LGUI")
+		float fps = 24;
+	UPROPERTY(EditAnywhere, Category = "LGUI")
+		bool loop = true;
+	/** Auto play when BeginPlay. */
+	UPROPERTY(EditAnywhere, Category = "LGUI")
+		bool playOnStart = true;
+	bool isPlaying = false;
+
+	virtual void BeginPlay()override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason)override;
+	FDelegateHandle playDelegateHandle;
+	void UpdateAnimation(float deltaTime);
+	virtual bool CanPlay() { return true; }
+	virtual void PrepareForPlay() {};
+	virtual void OnUpdateAnimation(int frameNumber)PURE_VIRTUAL(ULGUIImageSequencePlayer::OnUpdateAnimation, );
+	float elapsedTime = 0.0f;
+	float duration = 1.0f;
+	bool isPaused = false;
+public:
+	/** Play the animation sequence. If is paused then resume play */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void Play();
+	/** Stop the animation. */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void Stop();
+	/** Pause the animation, call Play to resume. */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void Pause() { isPaused = true; }
+	/** Seek to desired frame and play. */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SeekFrame(int frameNumber);
+	/** Seek to desired time and play. */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SeekTime(float time);
+	/** Is the animation playing? */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		bool GetIsPlaying()const { return isPlaying && !isPaused; }
+	/** Get the animation time length */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		virtual float GetDuration()const PURE_VIRTUAL(ULGUIImageSequencePlayer::GetDuration, return 0.0f;);
+
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		float GetFps()const { return fps; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		bool GetLoop()const { return loop; }
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetFps(float value);
+	/** Will take effect after current cycle. */
+	UFUNCTION(BlueprintCallable, Category = "LGUI")
+		void SetLoop(bool value);
+};
