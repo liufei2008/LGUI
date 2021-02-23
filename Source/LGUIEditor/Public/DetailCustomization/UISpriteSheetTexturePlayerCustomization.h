8dfa5e4b7a93854f3a176d084e03263c13e89c01
lf2007hf <707908214@qq.com>
2021-02-23 20:30:13 +0800

    Cleanup editor code
    (cherry picked from commit b30ef7ceafa7eb72d6e2370d73e5a2603831a609)

---
+++ Source/LGUIEditor/Public/DetailCustomization/UISpriteSheetTexturePlayerCustomization.h
@@ -0,0 +1,17 @@
+// Copyright 2019-2021 LexLiu. All Rights Reserved.
+
+#include "IDetailCustomization.h"
+#pragma once
+
+/**
+ * 
+ */
+class FUISpriteSequencePlayerCustomization : public IDetailCustomization
+{
+public:
+	static TSharedRef<IDetailCustomization> MakeInstance();
+	/** IDetailCustomization interface */
+	virtual void CustomizeDetails(IDetailLayoutBuilder& DetailBuilder) override;
+private:
+	TWeakObjectPtr<class UUISpriteSequencePlayer> TargetScriptPtr;
+};
