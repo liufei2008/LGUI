26cc9bf71ea883c601265abf4616e8a02de5cedf
lf2007hf <707908214@qq.com>
2021-02-23 20:30:00 +0800

    Add image sequence player, 2 new component: UISpriteSequencePlayer, UISpriteSheetTexturePlayer.
    (cherry picked from commit 2f18a43a4806c4b9944d34e5486a80b1fad1edd6)

---
+++ Source/LGUI/Private/Extensions/UISpriteSequencePlayer.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright 2019-2021 LexLiu. All Rights Reserved.
+
+#include "Extensions/UISpriteSequencePlayer.h"
+#include "LTweenBPLibrary.h"
+#include "Core/LGUISpriteData.h"
+#include "Core/ActorComponent/UISpriteBase.h"
+
+bool UUISpriteSequencePlayer::CanPlay()
+{
+	if (!sprite.IsValid())
+	{
+		sprite = GetOwner()->FindComponentByClass<UUISpriteBase>();
+	}
+	if (!sprite.IsValid())
+	{
+		UE_LOG(LGUI, Error, TEXT("[UUISpriteSequencePlayer::Play]Need UISprite component!"));
+		return false;
+	}
+	if (spriteSequence.Num() <= 0)
+	{
+		UE_LOG(LGUI, Error, TEXT("[UUISpriteSequencePlayer::Play]SpriteSequence array is empty!"));
+		return false;
+	}
+	return true;
+}
+float UUISpriteSequencePlayer::GetDuration()const
+{
+	return (float)(spriteSequence.Num()) / fps;
+}
+void UUISpriteSequencePlayer::PrepareForPlay()
+{
+
+}
+
+void UUISpriteSequencePlayer::OnUpdateAnimation(int frameNumber)
+{
+	frameNumber = FMath::Clamp(frameNumber, 0, spriteSequence.Num() - 1);
+	sprite->SetSprite(spriteSequence[frameNumber]);
+}
+
+void UUISpriteSequencePlayer::SetSpriteSequence(TArray<ULGUISpriteData*> value)
+{
+	spriteSequence = value;
+}
---
+++ Source/LGUI/Public/Extensions/UISpriteSheetTexturePlayer.h
@@ -0,0 +1,27 @@
+﻿// Copyright 2019-2021 LexLiu. All Rights Reserved.
+
+#pragma once
+#include "LGUIImageSequencePlayer.h"
+#include "UISpriteSheetTexturePlayer.generated.h"
+
+class ULGUISpriteData;
+
+UCLASS(ClassGroup = (LGUI), meta = (BlueprintSpawnableComponent))
+class LGUI_API UUISpriteSheetTexturePlayer : public ULGUIImageSequencePlayer
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(Transient)
+		TWeakObjectPtr<class UUITexture> texture;
+	UPROPERTY(EditAnywhere, Category = "LGUI")
+		int widthCount = 8;
+	UPROPERTY(EditAnywhere, Category = "LGUI")
+		int heightCount = 8;
+
+	float widthUVInterval, heightUVInterval;
+	virtual bool CanPlay()override;
+	virtual float GetDuration()const override;
+	virtual void PrepareForPlay()override;
+	virtual void OnUpdateAnimation(int frameNumber)override;
+public:
+};
