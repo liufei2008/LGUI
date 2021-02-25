e1975093bfb42e4477684f39f371518104cd9b54
lf2007hf <707908214@qq.com>
2021-02-25 10:58:56 +0800

    Add copy/paste on custom editor. Now we can copy/paste anchor of LGUITransform.
    (cherry picked from commit b8042a94cbe8fa7b10002f0fbe1a58577e1a6421)

--- Source/LGUIEditor/Public/DetailCustomization/UITextCustomization.h
+++ Source/LGUIEditor/Public/DetailCustomization/UITextCustomization.h
@@ -24,4 +24,11 @@ private:
 	ECheckBoxState GetHorizontalAlignmentCheckState(TSharedRef<IPropertyHandle> PropertyHandle, UITextParagraphHorizontalAlign ForAlignment) const;
 	void HandleVerticalAlignmentCheckStateChanged(ECheckBoxState InCheckboxState, TSharedRef<IPropertyHandle> PropertyHandle, UITextParagraphVerticalAlign ToAlignment);
 	ECheckBoxState GetVerticalAlignmentCheckState(TSharedRef<IPropertyHandle> PropertyHandle, UITextParagraphVerticalAlign ForAlignment) const;
+
+	void OnCopyText();
+	void OnPasteText(TSharedRef<IPropertyHandle> PropertyHandle);
+
+	void OnCopyAlignment();
+	void OnPasteAlignment(TSharedRef<IPropertyHandle> HAlignPropertyHandle, TSharedRef<IPropertyHandle> VAlignPropertyHandle);
+	bool OnCanPasteAlignment()const;
 };
