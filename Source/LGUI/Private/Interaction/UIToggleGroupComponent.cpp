079712cb75769c5b319e8e1d5f0c51a0f65d0ada
lf2007hf <707908214@qq.com>
2021-03-30 21:23:29 +0800

    Add callback events for UIToggleGroup.

--- Source/LGUI/Private/Interaction/UIToggleGroupComponent.cpp
+++ Source/LGUI/Private/Interaction/UIToggleGroupComponent.cpp
@@ -5,32 +5,62 @@
 #include "Interaction/UIToggleComponent.h"
 
 
-void UUIToggleGroupComponent::SelectItem(UUIToggleComponent* item)
+void UUIToggleGroupComponent::SetSelection(UUIToggleComponent* Target)
 {
-	if (LastSelect != item)
+	if (!IsValid(Target))
+	{
+		UE_LOG(LGUI, Error, TEXT("[UUIToggleGroupComponent::SelectItem]Toggle item is not valid!"));
+		return;
+	}
+	if (LastSelect.Get() != Target)
 	{
 		auto TempSelected = LastSelect;
-		LastSelect = item;
-		if (TempSelected != nullptr)
+		LastSelect = Target;
+		if (TempSelected.IsValid())
 		{
 			TempSelected->SetState(false);
 		}
+		if (OnToggleCPP.IsBound())OnToggleCPP.Broadcast(Target);
+		OnToggle.FireEvent(Target->GetOwner());
 	}
-}
-void UUIToggleGroupComponent::SelectNone()
-{
-	if (LastSelect != nullptr)
-		LastSelect->SetState(false);
-}
-void UUIToggleGroupComponent::SetSelection(UUIToggleComponent* Target)
-{
-	SelectItem(Target);
 }
 void UUIToggleGroupComponent::ClearSelection()
 {
-	SelectNone();
+	if (LastSelect.IsValid())
+	{
+		LastSelect->SetState(false);
+		LastSelect.Reset();
+
+		if (OnToggleCPP.IsBound())OnToggleCPP.Broadcast(nullptr);
+		OnToggle.FireEvent((AActor*)nullptr);
+	}
 }
 UUIToggleComponent* UUIToggleGroupComponent::GetSelectedItem()const
 {
 	return LastSelect.Get();
+}
+
+FDelegateHandle UUIToggleGroupComponent::RegisterToggleEvent(const FLGUIToggleGroupDelegate& InDelegate)
+{
+	return OnToggleCPP.Add(InDelegate);
+}
+FDelegateHandle UUIToggleGroupComponent::RegisterToggleEvent(const TFunction<void(UUIToggleComponent*)>& InFunction)
+{
+	return OnToggleCPP.AddLambda(InFunction);
+}
+void UUIToggleGroupComponent::UnregisterToggleEvent(const FDelegateHandle& InHandle)
+{
+	OnToggleCPP.Remove(InHandle);
+}
+
+FLGUIDelegateHandleWrapper UUIToggleGroupComponent::RegisterToggleEvent(const FLGUIToggleGroupDynamicDelegate& InDelegate)
+{
+	auto delegateHandle = OnToggleCPP.AddLambda([InDelegate](UUIToggleComponent* Value) {
+		if (InDelegate.IsBound())InDelegate.Execute(Value);
+		});
+	return FLGUIDelegateHandleWrapper(delegateHandle);
+}
+void UUIToggleGroupComponent::UnregisterToggleEvent(const FLGUIDelegateHandleWrapper& InDelegateHandle)
+{
+	OnToggleCPP.Remove(InDelegateHandle.DelegateHandle);
 }
