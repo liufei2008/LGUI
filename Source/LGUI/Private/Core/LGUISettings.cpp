13638e0d4021392f7861028a3fb16a5473a47bcd
lf2007hf <707908214@qq.com>
2021-04-01 08:32:20 +0800

    tiny modify
    (cherry picked from commit 7e81340ef37e065ed92fe5d56db9cc26631800e8)

--- Source/LGUI/Private/Core/LGUISettings.cpp
+++ Source/LGUI/Private/Core/LGUISettings.cpp
@@ -67,6 +67,10 @@ ELGUIScreenSpaceUIAntiAliasing ULGUISettings::GetAntiAliasingSampleCount()
 
 #if WITH_EDITOR
 FSimpleMulticastDelegate ULGUIEditorSettings::LGUIPreviewSetting_EditorPreviewViewportIndexChange;
+ULGUIEditorSettings::ULGUIEditorSettings()
+{
+	
+}
 void ULGUIEditorSettings::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
