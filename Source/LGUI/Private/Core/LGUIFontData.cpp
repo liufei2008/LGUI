e3e29e54f5d1188dd1587e9b8bd70072952565ab
lf2007hf <707908214@qq.com>
2021-05-23 10:10:39 +0800

    整理代码

--- Source/LGUI/Private/Core/LGUIFontData.cpp
+++ Source/LGUI/Private/Core/LGUIFontData.cpp
@@ -10,6 +10,7 @@
 #include "Core/LGUISettings.h"
 #include "Utils/LGUIUtils.h"
 #include "TextureResource.h"
+#include "Engine/Texture2D.h"
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include FT_OUTLINE_H
@@ -348,58 +349,6 @@ void ULGUIFontData::ApplyPackingAtlasTextureExpand(UTexture2D* newTexture, int n
 	}
 }
 
-void ULGUIFontData::UpdateTextureRegions(UTexture2D* Texture, int32 MipIndex, uint32 NumRegions, FUpdateTextureRegion2D* Regions, uint32 SrcPitch, uint32 SrcBpp, uint8* SrcData, bool bFreeData)
-{
-	if (Texture->Resource)
-	{
-		struct FUpdateTextureRegionsData
-		{
-			FTexture2DResource* Texture2DResource;
-			int32 MipIndex;
-			uint32 NumRegions;
-			FUpdateTextureRegion2D* Regions;
-			uint32 SrcPitch;
-			uint32 SrcBpp;
-			uint8* SrcData;
-		};
-
-		FUpdateTextureRegionsData* RegionData = new FUpdateTextureRegionsData;
-
-		RegionData->Texture2DResource = (FTexture2DResource*)Texture->Resource;
-		RegionData->MipIndex = MipIndex;
-		RegionData->NumRegions = NumRegions;
-		RegionData->Regions = Regions;
-		RegionData->SrcPitch = SrcPitch;
-		RegionData->SrcBpp = SrcBpp;
-		RegionData->SrcData = SrcData;
-		ENQUEUE_RENDER_COMMAND(FLGUIFontUpdateFontTextureRegionsData)(
-			[RegionData, bFreeData](FRHICommandListImmediate& RHICmdList)
-			{
-				for (uint32 RegionIndex = 0; RegionIndex < RegionData->NumRegions; ++RegionIndex)
-				{
-					int32 CurrentFirstMip = RegionData->Texture2DResource->GetCurrentFirstMip();
-					if (RegionData->MipIndex >= CurrentFirstMip)
-					{
-						RHIUpdateTexture2D(
-							RegionData->Texture2DResource->GetTexture2DRHI(),
-							RegionData->MipIndex - CurrentFirstMip,
-							RegionData->Regions[RegionIndex],
-							RegionData->SrcPitch,
-							RegionData->SrcData
-							+ RegionData->Regions[RegionIndex].SrcY * RegionData->SrcPitch
-							+ RegionData->Regions[RegionIndex].SrcX * RegionData->SrcBpp
-						);
-					}
-				}
-				if (bFreeData)
-				{
-					FMemory::Free(RegionData->SrcData);
-					FMemory::Free(RegionData->Regions);
-				}
-				delete RegionData;
-			});
-	}
-}
 void ULGUIFontData::UpdateFontTextureRegion(UTexture2D* Texture, FUpdateTextureRegion2D* Region, uint32 SrcPitch, uint32 SrcBpp, uint8* SrcData)
 {
 	if (Texture->Resource)
@@ -423,7 +372,7 @@ void ULGUIFontData::UpdateFontTextureRegion(UTexture2D* Texture, FUpdateTextureR
 		ENQUEUE_RENDER_COMMAND(FLGUIFontUpdateFontTextureRegionData)(
 			[RegionData](FRHICommandListImmediate& RHICmdList)
 			{
-				RHIUpdateTexture2D(
+				RHICmdList.UpdateTexture2D(
 					RegionData->Texture2DResource->GetTexture2DRHI(),
 					0,
 					*RegionData->Region,
