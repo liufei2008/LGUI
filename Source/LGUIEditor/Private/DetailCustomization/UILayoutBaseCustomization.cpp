0273f162b89a5fe2d186027d3655dce0afb2372d
lf2007hf <707908214@qq.com>
2021-06-02 08:12:01 +0800

    提示：多个UILayout控制同一个属性
    (cherry picked from commit 75b07131582b1e41a3233683e4ac248463e2b226)

--- Source/LGUIEditor/Private/DetailCustomization/UILayoutBaseCustomization.cpp
+++ Source/LGUIEditor/Private/DetailCustomization/UILayoutBaseCustomization.cpp
@@ -26,5 +26,60 @@ void FUILayoutBaseCustomization::CustomizeDetails(IDetailLayoutBuilder& DetailBu
 		UE_LOG(LGUIEditor, Log, TEXT("Get TargetScript is null"));
 	}
 	LGUIEditorUtils::ShowError_MultiComponentNotAllowed(&DetailBuilder, TargetScriptPtr.Get(), TEXT("Multiple UILayout component in one actor is not allowed!"));
+
+	bool anchorControlledByParentLayout = false;
+	bool widthControlledByParentLayout = false;
+	bool heightControlledByParentLayout = false;
+	bool anchorHControlledBySelfLayout = false;
+	bool anchorVControlledBySelfLayout = false;
+	bool widthControlledBySelfLayout = false;
+	bool heightControlledBySelfLayout = false;
+	bool stretchLeftControlledBySelfLayout = false;
+	bool stretchRightControlledBySelfLayout = false;
+	bool stretchTopControlledBySelfLayout = false;
+	bool stretchBottomControlledBySelfLayout = false;
+	if (auto thisActor = TargetScriptPtr->GetOwner())
+	{
+		if (auto parentActor = thisActor->GetAttachParentActor())
+		{
+			bool ignoreParentLayout = false;
+			if (auto thisLayoutElement = thisActor->FindComponentByClass<UUILayoutElement>())
+			{
+				ignoreParentLayout = thisLayoutElement->GetIgnoreLayout();
+			}
+			if (!ignoreParentLayout)
+			{
+				if (auto parentLayout = parentActor->FindComponentByClass<UUILayoutBase>())
+				{
+					anchorControlledByParentLayout = parentLayout->CanControlChildAnchor();
+					widthControlledByParentLayout = parentLayout->CanControlChildWidth();
+					heightControlledByParentLayout = parentLayout->CanControlChildHeight();
+				}
+			}
+		}
+		if (auto thisLayout = thisActor->FindComponentByClass<UUILayoutBase>())
+		{
+			anchorHControlledBySelfLayout = thisLayout->CanControlSelfHorizontalAnchor();
+			anchorVControlledBySelfLayout = thisLayout->CanControlSelfVerticalAnchor();
+			widthControlledBySelfLayout = thisLayout->CanControlSelfWidth();
+			heightControlledBySelfLayout = thisLayout->CanControlSelfHeight();
+			stretchLeftControlledBySelfLayout = thisLayout->CanControlSelfStrengthLeft();
+			stretchRightControlledBySelfLayout = thisLayout->CanControlSelfStrengthRight();
+			stretchTopControlledBySelfLayout = thisLayout->CanControlSelfStrengthTop();
+			stretchBottomControlledBySelfLayout = thisLayout->CanControlSelfStrengthBottom();
+		}
+	}
+	if (anchorControlledByParentLayout && (anchorHControlledBySelfLayout || anchorVControlledBySelfLayout))
+	{
+		LGUIEditorUtils::ShowWarning(&DetailBuilder, FString("Anchor is controlled by more than one UILayout component! This may cause issue!"));
+	}
+	if (widthControlledByParentLayout && widthControlledBySelfLayout)
+	{
+		LGUIEditorUtils::ShowWarning(&DetailBuilder, FString("Width is controlled by more than one UILayout component! This may cause issue!"));
+	}
+	if (heightControlledByParentLayout && heightControlledBySelfLayout)
+	{
+		LGUIEditorUtils::ShowWarning(&DetailBuilder, FString("Height is controlled by more than one UILayout component! This may cause issue!"));
+	}
 }
 #undef LOCTEXT_NAMESPACE
