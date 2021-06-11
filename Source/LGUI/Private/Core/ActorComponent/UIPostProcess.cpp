287a5d35f70f2caa4f656413e132962df5cf93a1
lf2007hf <707908214@qq.com>
2021-06-11 11:07:18 +0800

    修复安卓gles3.1不显示postprocess的问题。

--- Source/LGUI/Private/Core/ActorComponent/UIPostProcess.cpp
+++ Source/LGUI/Private/Core/ActorComponent/UIPostProcess.cpp
@@ -5,6 +5,7 @@
 #include "Core/ActorComponent/LGUICanvas.h"
 #include "Core/UIGeometry.h"
 #include "Core/UIPostProcessRenderProxy.h"
+#include "Core/LGUISpriteDataBaseObject.h"
 
 UUIPostProcess::UUIPostProcess(const FObjectInitializer& ObjectInitializer) :Super(ObjectInitializer)
 {
