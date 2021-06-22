115f1e7b2417d2f2274a0c923d6b36d597a9d251
lf2007hf <707908214@qq.com>
2021-06-22 12:02:29 +0800

    UICustomDepthMask重命名为UICustomDepthStencilMask

--- Source/LGUI/Public/Core/ActorComponent/UICustomDepthStencilMask.h
+++ Source/LGUI/Public/Core/ActorComponent/UICustomDepthStencilMask.h
@@ -4,10 +4,10 @@
 
 #include "UIPostProcess.h"
 #include "Core/HudRender/LGUIHudVertex.h"
-#include "UICustomDepthMask.generated.h"
+#include "UICustomDepthStencilMask.generated.h"
 
 UENUM(BlueprintType)
-enum class EUICustomDepthMaskSourceType :uint8
+enum class EUICustomDepthStencilMaskSourceType :uint8
 {
 	/** Use custom depth as mask */
 	CustomDepth,
@@ -20,12 +20,12 @@ enum class EUICustomDepthMaskSourceType :uint8
  * Android: CustomDepth mode not supported yet.
  */
 UCLASS(ClassGroup = (LGUI), NotBlueprintable, meta = (BlueprintSpawnableComponent))
-class LGUI_API UUICustomDepthMask : public UUIPostProcess
+class LGUI_API UUICustomDepthStencilMask : public UUIPostProcess
 {
 	GENERATED_BODY()
 
 public:	
-	UUICustomDepthMask(const FObjectInitializer& ObjectInitializer);
+	UUICustomDepthStencilMask(const FObjectInitializer& ObjectInitializer);
 
 protected:
 	virtual void BeginPlay() override;
@@ -34,11 +34,11 @@ protected:
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 #endif
 	/** Ignore UI item's size and do full screen effect. If SourceType==CustomStencil then this will always be true. */
-	UPROPERTY(EditAnywhere, Category = "LGUI", meta = (EditCondition="sourceType==EUICustomDepthMaskSourceType::CustomDepth"))
+	UPROPERTY(EditAnywhere, Category = "LGUI", meta = (EditCondition="sourceType==EUICustomDepthStencilMaskSourceType::CustomDepth"))
 		bool bFullScreen = true;
 	UPROPERTY(EditAnywhere, Category = "LGUI", meta = (ClampMin = "0.0", ClampMax = 1.0f))
-		EUICustomDepthMaskSourceType sourceType = EUICustomDepthMaskSourceType::CustomStencil;
-	UPROPERTY(EditAnywhere, Category = "LGUI", meta = (EditCondition="sourceType==EUICustomDepthMaskSourceType::CustomStencil"))
+		EUICustomDepthStencilMaskSourceType sourceType = EUICustomDepthStencilMaskSourceType::CustomStencil;
+	UPROPERTY(EditAnywhere, Category = "LGUI", meta = (EditCondition="sourceType==EUICustomDepthStencilMaskSourceType::CustomStencil"))
 		int stencilValue = 0;
 public:
 	virtual TWeakPtr<FUIPostProcessRenderProxy> GetRenderProxy()override;
@@ -47,14 +47,14 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		bool GetFullScreen()const { return bFullScreen; }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		EUICustomDepthMaskSourceType GetSourceType()const { return sourceType; }
+		EUICustomDepthStencilMaskSourceType GetSourceType()const { return sourceType; }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		int GetStencilValue()const { return stencilValue; }
 
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetFullScreen(bool value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
-		void SetSourceType(EUICustomDepthMaskSourceType value);
+		void SetSourceType(EUICustomDepthStencilMaskSourceType value);
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		void SetStencilValue(int value);
 protected:
