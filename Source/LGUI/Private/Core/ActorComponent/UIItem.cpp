bc63637fba0a993dfd6581d7a585fa13a246fd0d
lf2007hf <707908214@qq.com>
2021-07-25 21:55:47 +0800

    回退：允许不是RootComponent的UIItem接收射线碰撞
    (cherry picked from commit 37065d357d88257ed35cbb23908c8b86f48ab863)
    (cherry picked from commit 599f916f338881bd56e5cd4269a26c90e933ece0)

--- Source/LGUI/Private/Core/ActorComponent/UIItem.cpp
+++ Source/LGUI/Private/Core/ActorComponent/UIItem.cpp
@@ -1875,7 +1875,6 @@ bool UUIItem::LineTraceUI(FHitResult& OutHit, const FVector& Start, const FVecto
 	if (!IsUIActiveInHierarchy())return false;
 	if (!RenderCanvas.IsValid())return false;
 	if (!GetOwner())return false;
-	if (GetOwner()->GetRootComponent() != this)return false;//only root component can do line trace hit
 	auto inverseTf = GetComponentTransform().Inverse();
 	auto localSpaceRayOrigin = inverseTf.TransformPosition(Start);
 	auto localSpaceRayEnd = inverseTf.TransformPosition(End);
