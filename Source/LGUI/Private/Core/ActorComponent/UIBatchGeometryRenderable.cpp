5ca2c3a00eba724bdf3542e69aeb57b2ed1eb6d1
lf2007hf <707908214@qq.com>
2021-08-31 18:18:05 +0800

    修复:SetCustomUIMaterial材质未更新
    (cherry picked from commit 8dd927bdb4083e0ba285510cdd4096fe8c8a70d6)
    (cherry picked from commit c116c3d7edf00b8c2df8615f7a654f1bcd7020a6)
    (cherry picked from commit 7e4283186ef4e3f9a024c9d3661a0edc4ca5d707)

--- Source/LGUI/Private/Core/ActorComponent/UIBatchGeometryRenderable.cpp
+++ Source/LGUI/Private/Core/ActorComponent/UIBatchGeometryRenderable.cpp
@@ -217,6 +217,7 @@ void UUIBatchGeometryRenderable::MarkMaterialDirty()
 			{
 				//Remove from old drawcall, then add to new drawcall.
 				RenderCanvas->RemoveUIRenderable(this);
+				geometry->material = CustomUIMaterial;
 				RenderCanvas->AddUIRenderable(this);
 			}
 		}
