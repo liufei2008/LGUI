71a5530968d9fc38e67373b95db0d6c6e1db509a
lf2007hf <707908214@qq.com>
2021-10-10 17:07:14 +0800

    添加接口ILGUICultureChangedInterface来实现语言修改
    (cherry picked from commit 053aca1c34fc3a27d12eebfc32cc91134f61e393)

---
+++ Source/LGUI/Public/Core/ILGUICultureChangedInterface.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2019-2021 LexLiu. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Event/LGUIPointerEventData.h"
+#include "ILGUICultureChangedInterface.generated.h"
+
+
+/**
+ * Interface for handling LGUI's culture changed.
+ * Need to register UObject with RegisterLGUICultureChangedEvent, check UIText for reference
+ */
+UINTERFACE(Blueprintable, MinimalAPI)
+class ULGUICultureChangedInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+/**
+ * Interface for handling LGUI's culture changed.
+ * Need to register UObject with RegisterLGUICultureChangedEvent, check UIText for reference
+ */ 
+class LGUI_API ILGUICultureChangedInterface
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * Called when current culture changed and LGUI need to update culture.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = LGUI)
+		void OnCultureChanged();
+};
