5f3488cbfde54857b0a9aed414ab37b121ddce13
lf2007hf <707908214@qq.com>
2022-02-28 18:46:13 +0800

    version to 3.0.3

--- Source/LGUI/Private/PrefabAnimation/LGUIPrefabSequenceComponent.cpp
+++ Source/LGUI/Private/PrefabAnimation/LGUIPrefabSequenceComponent.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2019-2022 LexLiu. All Rights Reserved.
 
 #include "PrefabAnimation/LGUIPrefabSequenceComponent.h"
 #include "PrefabAnimation/LGUIPrefabSequence.h"
