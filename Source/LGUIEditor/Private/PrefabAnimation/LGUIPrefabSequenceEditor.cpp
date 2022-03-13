51fdd1921bb0626abe56b2fd9e459faaa79b2983
lf2007hf <707908214@qq.com>
2022-03-13 08:10:43 +0800

    修复bug：重命名之后丢失所有track。 修复bug：重命名时如果内存里已经存在相同名称的object就提示。

--- Source/LGUIEditor/Private/PrefabAnimation/LGUIPrefabSequenceEditor.cpp
+++ Source/LGUIEditor/Private/PrefabAnimation/LGUIPrefabSequenceEditor.cpp
@@ -98,6 +98,12 @@ private:
 		auto Animation = ListItem.Pin()->Animation;
 
 		const FName NewName = *InText.ToString().Left(NAME_SIZE - 1);
+		UObject* ExistingObject = StaticFindObject(NULL, Animation->GetOuter(), *NewName.ToString(), true);
+		if (ExistingObject != nullptr && ExistingObject != Animation)
+		{
+			OutErrorMessage = LOCTEXT("NameInUseByAnimation", "An animation with this name already exists");
+			return false;
+		}
 
 		if (Animation->GetFName() != NewName)
 		{
@@ -143,10 +149,8 @@ private:
 			{
 				const FScopedTransaction Transaction(TransactionName);
 				Animation->Modify();
-				Animation->GetMovieScene()->Modify();
 
 				Animation->Rename(*NewNameStr);
-				Animation->GetMovieScene()->Rename(*NewNameStr);
 
 				if (bNewAnimation)
 				{
