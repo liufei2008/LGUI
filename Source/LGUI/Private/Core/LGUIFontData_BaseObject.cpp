c37eb4714b8de081d4aea7f986f21c87cddd7e22
lf2007hf <707908214@qq.com>
2022-05-20 17:06:12 +0800

    默认字体改为SDF

--- Source/LGUI/Private/Core/LGUIFontData_BaseObject.cpp
+++ Source/LGUI/Private/Core/LGUIFontData_BaseObject.cpp
@@ -3,3 +3,13 @@
 #include "Core/LGUIFontData_BaseObject.h"
 #include "LGUI.h"
 
+ULGUIFontData_BaseObject* ULGUIFontData_BaseObject::GetDefaultFont()
+{
+	static auto defaultFont = LoadObject<ULGUIFontData_BaseObject>(NULL, TEXT("/LGUI/DefaultSDFFont"));
+	if (defaultFont == nullptr)
+	{
+		UE_LOG(LGUI, Error, TEXT("[ULGUIFontData_BaseObject::GetDefaultFont]Load default font error! Missing some content of LGUI plugin, reinstall this plugin may fix the issure."));
+		return nullptr;
+	}
+	return defaultFont;
+}
