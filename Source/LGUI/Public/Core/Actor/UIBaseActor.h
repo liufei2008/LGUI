7e737f6c1d2debbcc93c2236bc62bec0c0d26efb
lf2007hf <707908214@qq.com>
2022-06-29 08:12:01 +0800

    添加回GetUIItem_BP，防止蓝图报错

--- Source/LGUI/Public/Core/Actor/UIBaseActor.h
+++ Source/LGUI/Public/Core/Actor/UIBaseActor.h
@@ -20,6 +20,9 @@ public:
 	/** This is for blueprint actor which inherit UIBaseActor, Implement it and return UIItem. */
 	UFUNCTION(BlueprintNativeEvent, Category = "LGUI")
 		UUIItem* GetUIItem_Impl()const;
+	UE_DEPRECATED(4.26, "Use GetUIItem instead.")
+	UFUNCTION(BlueprintCallable, Category = "LGUI", meta = (DeprecatedFunction, DeprecationMessage = "Use GetUIItem instead."))
+		UUIItem* GetUIItem_BP()const { return GetUIItem(); }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		virtual UUIItem* GetUIItem()const PURE_VIRTUAL(AUIBaseActor::GetUIItem, return nullptr;);
 private:
@@ -37,6 +40,9 @@ public:
 	/** This is for blueprint actor which inherit UIBaseRenderableActor, Implement it and return UIBaseRenderable. */
 	UFUNCTION(BlueprintNativeEvent, Category = "LGUI")
 		UUIBaseRenderable* GetUIRenderable_Impl()const;
+	UE_DEPRECATED(4.26, "Use GetUIRenderable instead.")
+	UFUNCTION(BlueprintCallable, Category = "LGUI", meta = (DeprecatedFunction, DeprecationMessage = "Use GetUIRenderable instead."))
+		UUIBaseRenderable* GetUIRenderable_BP()const { return GetUIRenderable(); }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		virtual UUIBaseRenderable* GetUIRenderable()const PURE_VIRTUAL(AUIBaseRenderableActor::GetUIRenderable, return nullptr;);
 	virtual UUIItem* GetUIItem()const override;
@@ -55,6 +61,9 @@ public:
 	/** This is for blueprint actor which inherit UIBasePostProcessActor, Implement it and return UIPostProcessRenderable. */
 	UFUNCTION(BlueprintNativeEvent, Category = "LGUI")
 		UUIPostProcessRenderable* GetUIPostProcessRenderable_Impl()const;
+	UE_DEPRECATED(4.26, "Use GetUIPostProcessRenderable instead.")
+	UFUNCTION(BlueprintCallable, Category = "LGUI", meta = (DeprecatedFunction, DeprecationMessage = "Use GetUIPostProcessRenderable instead."))
+		UUIPostProcessRenderable* GetUIPostProcessRenderable_BP()const { return GetUIPostProcessRenderable(); }
 	UFUNCTION(BlueprintCallable, Category = "LGUI")
 		virtual UUIPostProcessRenderable* GetUIPostProcessRenderable()const PURE_VIRTUAL(AUIBasePostProcessActor::GetUIPostProcessRenderable, return nullptr;);
 	virtual UUIBaseRenderable* GetUIRenderable()const override;
