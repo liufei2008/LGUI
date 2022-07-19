c2dee33f68f83b7ac916808e0ad96c0287181dad
lf2007hf <707908214@qq.com>
2022-07-20 07:53:33 +0800

    修复：拖拽Prefab到场景生成Actor失效，由LoadingPhase引起。解决办法：把ComponentVisualizer单独放到一个模块，LGUIEditor的LoadingPhase就可以改为Default。

---
+++ Source/LGUIComponentVisualizer/LGUIComponentVisualizer.Build.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2019-2022 LexLiu. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LGUIComponentVisualizer : ModuleRules
+{
+	public LGUIComponentVisualizer(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateIncludePaths.AddRange(
+                new string[] {
+                    
+                });
+
+        PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "CoreUObject",
+                "Slate",
+                "SlateCore",
+                "Engine",
+                "UnrealEd",
+                "LGUI",
+            }
+            );
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+                "EditorStyle",
+				// ... add private dependencies that you statically link with here ...	
+
+            }
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+    }
+}
---
+++ Source/LGUIComponentVisualizer/Private/LGUIComponentVisualizerModule.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright 2019-2022 LexLiu. All Rights Reserved.
+
+#include "LGUIComponentVisualizerModule.h"
+
+#include "LGUIHeaders.h"
+#include "ComponentVisualizer/UIFlexibleGridLayoutComponentVisualizer.h"
+#include "UnrealEdGlobals.h"
+#include "Editor/UnrealEdEngine.h"
+
+#define LOCTEXT_NAMESPACE "FLGUIComponentVisualizerModule"
+DEFINE_LOG_CATEGORY(LGUIComponentVisualizer);
+
+void FLGUIComponentVisualizerModule::StartupModule()
+{
+	//component visualizer
+	{
+		TSharedPtr<FUIFlexibleGridLayoutComponentVisualizer> Visualizer = MakeShareable(new FUIFlexibleGridLayoutComponentVisualizer);
+		if (GUnrealEd)
+		{
+			GUnrealEd->RegisterComponentVisualizer(UUIFlexibleGridLayout::StaticClass()->GetFName(), Visualizer);
+			Visualizer->OnRegister();
+		}
+	}
+}
+
+void FLGUIComponentVisualizerModule::ShutdownModule()
+{
+	//unregister component visualizer
+	{
+		if (GUnrealEd)
+		{
+			GUnrealEd->UnregisterComponentVisualizer(UUIFlexibleGridLayout::StaticClass()->GetFName());
+		}
+	}
+}
+
+IMPLEMENT_MODULE(FLGUIComponentVisualizerModule, LGUIComponentVisualizer)
+
+#undef LOCTEXT_NAMESPACE
