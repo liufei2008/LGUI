b6cb3a7d5b2ff404f9cb0dd7d1093e44a0a0a32e
lf2007hf <707908214@qq.com>
2023-01-12 21:23:55 +0800

    tiny fix

--- Source/LGUIEditor/Public/LGUIEditorCommands.cpp
+++ Source/LGUIEditor/Public/LGUIEditorCommands.cpp
@@ -24,7 +24,7 @@ void FLGUIEditorCommands::RegisterCommands()
 	UI_COMMAND(FocusToScreenSpaceUI, "Focus to Screen-Space-UI", "Make the editor viewport's camera face to Screen-Space-UI (If there is a screen space UI)", EUserInterfaceActionType::Button, FInputChord(EKeys::F, EModifierKey::Alt | EModifierKey::Shift));
 	UI_COMMAND(FocusToSelectedUI, "Focus to Selected UI", "Make the editor viewport's camera face to selected UI", EUserInterfaceActionType::Button, FInputChord());
 
-	UI_COMMAND(ActiveViewportAsLGUIPreview, "Active Viewport as ScreenSpaceUI Preview", "Use current selected active editor viewport for Screen-Space-UI preview", EUserInterfaceActionType::ToggleButton, FInputChord(EKeys::V, EModifierKey::Alt | EModifierKey::Shift));
+	UI_COMMAND(ActiveViewportAsLGUIPreview, "Active Viewport as ScreenSpaceUI Preview", "Use current selected active editor viewport for Screen-Space-UI preview", EUserInterfaceActionType::ToggleButton, FInputChord());
 	UI_COMMAND(ToggleLGUIInfoColume, "Show LGUI column in SceneOutliner", "Show LGUI column in SceneOutliner", EUserInterfaceActionType::ToggleButton, FInputChord());
 	UI_COMMAND(ToggleDrawHelperFrame, "Draw helper box on selected UI element", "Draw helper box on selected UI element", EUserInterfaceActionType::ToggleButton, FInputChord());
 	UI_COMMAND(ForceGC, "ForceGC", "Force garbage collection immediately, this is useful in some test work", EUserInterfaceActionType::Button, FInputChord());
