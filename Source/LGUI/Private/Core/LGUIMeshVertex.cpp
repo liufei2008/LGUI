2d5d6ca92bb30933a43821ff09bb298ab890996e
lf2007hf <707908214@qq.com>
2023-05-01 06:12:42 +0800

    tiny modify

--- Source/LGUI/Private/Core/LGUIMeshVertex.cpp
+++ Source/LGUI/Private/Core/LGUIMeshVertex.cpp
@@ -7,14 +7,15 @@ void FLGUIMeshVertexDeclaration::InitRHI()
 {
 	FVertexDeclarationElementList Elements;
 	uint32 Stride = sizeof(FLGUIMeshVertex);
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, Position), VET_Float3, 0, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, Color), VET_Color, 1, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TextureCoordinate) + 0, VET_Float2, 2, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TextureCoordinate) + 8, VET_Float2, 3, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TextureCoordinate) + 16, VET_Float2, 4, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TextureCoordinate) + 24, VET_Float2, 5, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TangentX), VET_PackedNormal, 6, Stride));
-	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TangentZ), VET_PackedNormal, 7, Stride));
+	uint16 Index = 0;
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, Position), VET_Float3, Index++, Stride));
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, Color), VET_Color, Index++, Stride));
+	for (int i = 0; i < LGUI_VERTEX_TEXCOORDINATE_COUNT; i++)
+	{
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TextureCoordinate) + i * 8, VET_Float2, Index++, Stride));
+	}
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TangentX), VET_PackedNormal, Index++, Stride));
+	Elements.Add(FVertexElement(0, STRUCT_OFFSET(FLGUIMeshVertex, TangentZ), VET_PackedNormal, Index++, Stride));
 	VertexDeclarationRHI = RHICreateVertexDeclaration(Elements);
 }
 void FLGUIMeshVertexDeclaration::ReleaseRHI()
