69c316a7f54c7c847ddce7e485233569faa22e16
lf2007hf <707908214@qq.com>
2023-06-20 12:36:06 +0800

    完善，添加函数

--- Source/LGUI/Private/Core/ActorComponent/UIProceduralRect.cpp
+++ Source/LGUI/Private/Core/ActorComponent/UIProceduralRect.cpp
@@ -22,6 +22,7 @@ FName UUIProceduralRect::DataTextureParameterName = TEXT("DataTexture");
 UUIProceduralRect::UUIProceduralRect(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
 {
 	PrimaryComponentTick.bCanEverTick = false;
+	bNeedUpdateBlockData = true;
 }
 
 void UUIProceduralRect::BeginPlay()
@@ -109,6 +110,12 @@ void UUIProceduralRect::OnMaterialInstanceDynamicCreated(class UMaterialInstance
 	mat->SetTextureParameterValue(DataTextureParameterName, ProceduralRectData->GetDataTexture());
 }
 
+void UUIProceduralRect::MarkAllDirty()
+{
+	Super::MarkAllDirty();
+	bNeedUpdateBlockData = true;
+}
+
 void UUIProceduralRect::CheckAdditionalShaderChannels()
 {
 	if (RenderCanvas.IsValid())
@@ -151,7 +158,7 @@ void UUIProceduralRect::OnUpdateGeometry(UIGeometry& InGeo, bool InTriangleChang
 {
 	static FLGUISpriteInfo SimpleRectSpriteData;
 	UIGeometry::UpdateUIProceduralRectSimpleVertex(&InGeo,
-		this->BlockData.bOuterShadow, this->BlockData.OuterShadowOffset, this->BlockData.OuterShadowSize, this->BlockData.OuterShadowBlur,
+		this->BlockData.bEnableOuterShadow, this->BlockData.OuterShadowOffset, this->BlockData.OuterShadowSize, this->BlockData.OuterShadowBlur,
 		this->GetWidth(), this->GetHeight(), FVector2f(this->GetPivot()), SimpleRectSpriteData, RenderCanvas.Get(), this, GetFinalColor(),
 		InTriangleChanged, InVertexPositionChanged, InVertexUVChanged, InVertexColorChanged
 	);
@@ -159,7 +166,7 @@ void UUIProceduralRect::OnUpdateGeometry(UIGeometry& InGeo, bool InTriangleChang
 	if (InTriangleChanged || InVertexPositionChanged || InVertexUVChanged || InVertexColorChanged)
 	{
 		auto& vertices = InGeo.vertices;
-		if (this->BlockData.bOuterShadow)
+		if (this->BlockData.bEnableOuterShadow)
 		{
 			for (int i = 0; i < 8; i++)
 			{
@@ -178,7 +185,12 @@ void UUIProceduralRect::OnUpdateGeometry(UIGeometry& InGeo, bool InTriangleChang
 				vertices[i].TextureCoordinate[2] = FVector2f(0, 0);
 			}
 		}
+		bNeedUpdateBlockData = true;
+	}
 
+	if (bNeedUpdateBlockData)
+	{
+		bNeedUpdateBlockData = false;
 
 		BlockData.QuadSize.X = this->GetWidth() * 0.5f;
 		BlockData.QuadSize.Y = this->GetHeight() * 0.5f;
@@ -194,6 +206,172 @@ void UUIProceduralRect::OnUpdateGeometry(UIGeometry& InGeo, bool InTriangleChang
 void UUIProceduralRect::OnAnchorChange(bool InPivotChange, bool InWidthChange, bool InHeightChange, bool InDiscardCache)
 {
 	Super::OnAnchorChange(InPivotChange, InWidthChange, InHeightChange, InDiscardCache);
+}
+
+void UUIProceduralRect::SetBlockData(const FUIProceduralRectBlockData& value)
+{
+	BlockData = value;
+	MarkAllDirty();
+}
+
+void UUIProceduralRect::SetCornerRadius(const FVector4f& value)
+{
+	BlockData.CornerRadius = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetTexture(UTexture* value)
+{
+	BlockData.Texture = value;
+	if (BlockData.Texture == nullptr)
+	{
+		BlockData.Texture = UUITextureBase::GetDefaultWhiteTexture();
+	}
+	MarkTextureDirty();
+}
+void UUIProceduralRect::SetTextureScaleMode(EUIProceduralRectTextureScaleMode value)
+{
+	BlockData.TextureScaleMode = value;
+	bNeedUpdateBlockData = true;
+}
+
+void UUIProceduralRect::SetEnableGradient(bool value)
+{
+	BlockData.bEnableGradient = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetGradientColor(const FColor& value)
+{
+	BlockData.GradientColor = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetGradientCenter(const FVector2f& value)
+{
+	BlockData.GradientCenter = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetGradientRadius(const FVector2f& value)
+{
+	BlockData.GradientRadius = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetGradientRotation(float value)
+{
+	BlockData.GradientRotation = value;
+	bNeedUpdateBlockData = true;
+}
+
+void UUIProceduralRect::SetEnableBorder(bool value)
+{
+	BlockData.bEnableBorder = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderWidth(float value)
+{
+	BlockData.BorderWidth = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderColor(const FColor& value)
+{
+	BlockData.BorderColor = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetEnableBorderGradient(bool value)
+{
+	BlockData.bEnableBorderGradient = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderGradientColor(const FColor& value)
+{
+	BlockData.BorderGradientColor = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderGradientCenter(const FVector2f& value)
+{
+	BlockData.BorderGradientCenter = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderGradientRadius(const FVector2f& value)
+{
+	BlockData.BorderGradientRadius = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetBorderGradientRotation(float value)
+{
+	BlockData.BorderGradientRotation = value;
+	bNeedUpdateBlockData = true;
+}
+
+void UUIProceduralRect::SetEnableInnerShadow(bool value)
+{
+	BlockData.bEnableInnerShadow = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetInnerShadowColor(const FColor& value)
+{
+	BlockData.InnerShadowColor = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetInnerShadowSize(float value)
+{
+	BlockData.InnerShadowSize = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetInnerShadowBlur(float value)
+{
+	BlockData.InnerShadowBlur = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetInnerShadowOffset(const FVector2f& value)
+{
+	BlockData.InnerShadowOffset = value;
+	bNeedUpdateBlockData = true;
+}
+
+void UUIProceduralRect::SetEnableRadialFill(bool value)
+{
+	BlockData.bEnableRadialFill = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetRadialFillCenter(const FVector2f& value)
+{
+	BlockData.RadialFillCenter = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetRadialFillRotation(float value)
+{
+	BlockData.RadialFillRotation = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetRadialFillAngle(float value)
+{
+	BlockData.RadialFillAngle = value;
+	bNeedUpdateBlockData = true;
+}
+
+void UUIProceduralRect::SetEnableOuterShadow(bool value)
+{
+	BlockData.bEnableOuterShadow = value;
+	MarkAllDirty();
+}
+void UUIProceduralRect::SetOuterShadowColor(const FColor& value)
+{
+	BlockData.OuterShadowColor = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetOuterShadowSize(float value)
+{
+	BlockData.OuterShadowSize = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetOuterShadowBlur(float value)
+{
+	BlockData.OuterShadowBlur = value;
+	bNeedUpdateBlockData = true;
+}
+void UUIProceduralRect::SetOuterShadowOffset(const FVector2f& value)
+{
+	BlockData.OuterShadowOffset = value;
+	bNeedUpdateBlockData = true;
 }
 
 #undef LOCTEXT_NAMESPACE
