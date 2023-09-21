5e9cc9451395d423412389d083a3c02c91ffd672
lf2007hf <707908214@qq.com>
2023-09-21 14:15:11 +0800

    LGUIEventDelegateParameterType改名为ELGUIEventDelegateParameterType

--- Source/LGUI/Public/PlayTween/LGUIPlayTween_Params.h
+++ Source/LGUI/Public/PlayTween/LGUIPlayTween_Params.h
@@ -5,7 +5,7 @@
 #include "LGUIPlayTween_Params.generated.h"
 
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Float"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Float"))
 class LGUI_API ULGUIPlayTween_Float : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -17,7 +17,7 @@ protected:
 		float to = 1.0f;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Float);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Float);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -25,7 +25,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Double"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Double"))
 class LGUI_API ULGUIPlayTween_Double : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -37,7 +37,7 @@ protected:
 		double to = 1.0f;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Double);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Double);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -45,7 +45,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Color"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Color"))
 class LGUI_API ULGUIPlayTween_Color : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -57,7 +57,7 @@ protected:
 		FColor to = FColor::Green;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Color);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Color);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -70,7 +70,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Int"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Int"))
 class LGUI_API ULGUIPlayTween_Int : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -82,7 +82,7 @@ protected:
 		int to = 100;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Int32);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Int32);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -90,7 +90,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween LinearColor"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween LinearColor"))
 class LGUI_API ULGUIPlayTween_LinearColor : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -102,7 +102,7 @@ protected:
 		FLinearColor to = FLinearColor::Green;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::LinearColor);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::LinearColor);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -110,7 +110,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Quaternion"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Quaternion"))
 class LGUI_API ULGUIPlayTween_Quaternion : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -122,7 +122,7 @@ protected:
 		FQuat to = FQuat(FVector(0.0f, 0.0f, 1.0f), HALF_PI);
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Quaternion);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Quaternion);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -130,7 +130,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Rotator"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Rotator"))
 class LGUI_API ULGUIPlayTween_Rotator : public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -142,7 +142,7 @@ protected:
 		FRotator to = FRotator(0.0f, 0.0f, 90.0f);
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Rotator);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Rotator);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -150,7 +150,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Vector2"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Vector2"))
 class LGUI_API ULGUIPlayTween_Vector2: public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -162,7 +162,7 @@ protected:
 		FVector2D to = FVector2D(1.0f, 1.0f);
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Vector2);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Vector2);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -170,7 +170,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Vector3"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Vector3"))
 class LGUI_API ULGUIPlayTween_Vector3: public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -182,7 +182,7 @@ protected:
 		FVector to = FVector::OneVector;
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Vector3);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Vector3);
 
 	virtual void OnUpdate(float progress)override
 	{
@@ -190,7 +190,7 @@ protected:
 	}
 };
 
-UCLASS(BlueprintType, meta = (DisplayName = "LGUIPLayTween Vector4"))
+UCLASS(BlueprintType, meta = (DisplayName = "LGUIPlayTween Vector4"))
 class LGUI_API ULGUIPlayTween_Vector4: public ULGUIPlayTween
 {
 	GENERATED_BODY()
@@ -202,7 +202,7 @@ protected:
 		FVector4 to = FVector4(1.0f, 1.0f, 1.0f, 1.0f);
 	/** parameter float is interpolated value from->to */
 	UPROPERTY(EditAnywhere, Category = "Event")
-		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(LGUIEventDelegateParameterType::Vector4);
+		FLGUIEventDelegate onUpdateValue = FLGUIEventDelegate(ELGUIEventDelegateParameterType::Vector4);
 
 	virtual void OnUpdate(float progress)override
 	{
