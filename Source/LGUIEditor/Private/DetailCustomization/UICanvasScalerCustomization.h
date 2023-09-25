ff6e92e680d84909860296ecbc0bb82442b89a27
lf2007hf <707908214@qq.com>
2023-09-25 17:53:41 +0800

    添加LGUICanvasScalerCustomScale用于自定义LGUI的尺寸和缩放

--- Source/LGUIEditor/Private/DetailCustomization/UICanvasScalerCustomization.h
+++ Source/LGUIEditor/Private/DetailCustomization/UICanvasScalerCustomization.h
@@ -15,8 +15,6 @@ public:
 	virtual void CustomizeDetails(IDetailLayoutBuilder& DetailBuilder) override;
 private:
 	TWeakObjectPtr<class ULGUICanvasScaler> TargetScriptPtr;
-	float GetMatchValue(TSharedRef<IPropertyHandle> Property)const;
-	void SetMatchValue(float value, TSharedRef<IPropertyHandle> Property);
 	TSharedPtr<SHorizontalBox> ValueBox;
 	FOptionalSize GetValueWidth()const;
 };
