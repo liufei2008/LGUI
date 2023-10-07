b85a50febd48ec88dc4dbff3f615c5e950ba9f9c
lf2007hf <707908214@qq.com>
2023-10-07 11:43:17 +0800

    fix warning for 5.3

--- Source/LGUIEditor/Private/PrefabEditor/LGUIPrefabEditorViewportClient.cpp
+++ Source/LGUIEditor/Private/PrefabEditor/LGUIPrefabEditorViewportClient.cpp
@@ -191,33 +191,24 @@ void FLGUIPrefabEditorViewportClient::Draw(const FSceneView* View, FPrimitiveDra
 
 	//AGroupActor::DrawBracketsForGroups(PDI, Viewport);
 
-	// Determine if a view frustum should be rendered in the viewport.
-	// The frustum should definitely be rendered if the viewport has a view parent.
-	bool bRenderViewFrustum = ViewState.GetReference()->HasViewParent();
-
-	// If the viewport doesn't have a view parent, a frustum still should be drawn anyway if the viewport is ortho and level streaming
-	// volume previs is enabled in some viewport
-	if (!bRenderViewFrustum && IsOrtho())
+	// A frustum should be drawn if the viewport is ortho and level streaming volume previs is enabled in some viewport
+	if (IsOrtho())
 	{
 		for (FLevelEditorViewportClient* CurViewportClient : GEditor->GetLevelViewportClients())
 		{
 			if (CurViewportClient && IsPerspective() && GetDefault<ULevelEditorViewportSettings>()->bLevelStreamingVolumePrevis)
 			{
-				bRenderViewFrustum = true;
+				// Draw the view frustum of the level streaming volume previs viewport.
+				RenderViewFrustum(PDI, FLinearColor(1.0, 0.0, 1.0, 1.0),
+					GPerspFrustumAngle,
+					GPerspFrustumAspectRatio,
+					GPerspFrustumStartDist,
+					GPerspFrustumEndDist,
+					GPerspViewMatrix);
+
 				break;
 			}
 		}
-	}
-
-	// Draw the view frustum of the view parent or level streaming volume previs viewport, if necessary
-	if (bRenderViewFrustum)
-	{
-		RenderViewFrustum(PDI, FLinearColor(1.0, 0.0, 1.0, 1.0),
-			GPerspFrustumAngle,
-			GPerspFrustumAspectRatio,
-			GPerspFrustumStartDist,
-			GPerspFrustumEndDist,
-			GPerspViewMatrix);
 	}
 
 	if (GEditor->bEnableSocketSnapping)
