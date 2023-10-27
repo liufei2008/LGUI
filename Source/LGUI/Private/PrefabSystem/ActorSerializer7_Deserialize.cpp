288c6cd8be21fda64490997eea8a95c6cecf767b
lf2007hf <707908214@qq.com>
2023-10-27 10:20:26 +0800

    修复：创建预制体失败导致崩溃

--- Source/LGUI/Private/PrefabSystem/ActorSerializer7_Deserialize.cpp
+++ Source/LGUI/Private/PrefabSystem/ActorSerializer7_Deserialize.cpp
@@ -217,6 +217,17 @@ namespace LGUIPrefabSystem7
 			}
 		}
 		auto CreatedRootActor = GenerateActorArray(SaveData.SavedActors, SaveData.SavedObjects, SaveData.MapSceneComponentToParent, FGuid());
+		if (CreatedRootActor == nullptr)
+		{
+			UE_LOG(LGUI, Error, TEXT("[%s].%d No actor generated!"), ANSI_TO_TCHAR(__FUNCTION__), __LINE__);
+
+			if (!bIsSubPrefab)
+			{
+				check(DeserializationSessionId.IsValid());
+				LGUIManagerActor->EndPrefabSystemProcessingActor(DeserializationSessionId);
+			}
+			return nullptr;
+		}
 		GenerateObjectArray(SaveData.SavedObjects, SaveData.MapSceneComponentToParent);
 #if LGUIPREFAB_LOG_DETAIL_TIME
 		UE_LOG(LGUI, Log, TEXT("--GenerateObject take time: %fms"), (FDateTime::Now() - Time).GetTotalMilliseconds());
@@ -305,39 +316,36 @@ namespace LGUIPrefabSystem7
 		}
 
 		//attach root actor's parent
-		if (CreatedRootActor != nullptr)
+		if (USceneComponent* RootComp = CreatedRootActor->GetRootComponent())
 		{
-			if (USceneComponent* RootComp = CreatedRootActor->GetRootComponent())
+			if (Parent)
 			{
-				if (Parent)
+				RootComp->AttachToComponent(Parent, FAttachmentTransformRules::KeepRelativeTransform);
+			}
+			//for UI
+			{
+				auto RootUIComp = Cast<UUIItem>(RootComp);
+				if (RootUIComp)//if UIItem have parent, CheckUIActiveState will becalled when attach
 				{
-					RootComp->AttachToComponent(Parent, FAttachmentTransformRules::KeepRelativeTransform);
-				}
-				//for UI
-				{
-					auto RootUIComp = Cast<UUIItem>(RootComp);
-					if (RootUIComp)//if UIItem have parent, CheckUIActiveState will becalled when attach
+					if (Parent)
 					{
-						if (Parent)
+						//recreate hierarchy index
+						if (bSetHierarchyIndexForRootComponent)
 						{
-							//recreate hierarchy index
-							if (bSetHierarchyIndexForRootComponent)
-							{
-								RootUIComp->SetAsLastHierarchy();
-							}
-						}
-						else
-						{
-							RootUIComp->CheckUIActiveState();//for UIItem not have parent, need to CheckUIActiveState
+							RootUIComp->SetAsLastHierarchy();
 						}
 					}
+					else
+					{
+						RootUIComp->CheckUIActiveState();//for UIItem not have parent, need to CheckUIActiveState
+					}
 				}
-				RootComp->UpdateComponentToWorld();
-				if (ReplaceTransform)
-				{
-					RootComp->SetRelativeLocationAndRotation(InLocation, InRotation);
-					RootComp->SetRelativeScale3D(InScale);
-				}
+			}
+			RootComp->UpdateComponentToWorld();
+			if (ReplaceTransform)
+			{
+				RootComp->SetRelativeLocationAndRotation(InLocation, InRotation);
+				RootComp->SetRelativeScale3D(InScale);
 			}
 		}
 
