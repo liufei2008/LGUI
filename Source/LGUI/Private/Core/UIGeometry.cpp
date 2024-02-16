b80735cd3fe09010cc761c38feaed9e940766f06
lf2007hf <707908214@qq.com>
2024-02-16 19:02:53 +0800

    优化sdf文字效果：斜方向看文字的时候消除锯齿。 屏幕空间UI的默认fov改为60，之前是90会导致sdf文字在屏幕边缘的显示不够锐利。

--- Source/LGUI/Private/Core/UIGeometry.cpp
+++ Source/LGUI/Private/Core/UIGeometry.cpp
@@ -2977,6 +2977,19 @@ void UIGeometry::UpdateUIText(const FString& text, int32 visibleCharCount, float
 		}
 	}
 
+	//additional data
+	{
+		//normal & tangent
+		if (renderCanvas->GetRequireNormal() || renderCanvas->GetRequireTangent())
+		{
+			for (int i = 0; i < originVertices.Num(); i++)
+			{
+				originVertices[i].Normal = FVector3f(-1, 0, 0);
+				originVertices[i].Tangent = FVector3f(0, 1, 0);
+			}
+		}
+	}
+
 	//verify custom tag
 	if (richText)
 	{
