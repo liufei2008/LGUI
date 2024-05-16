85e2978688b9de38f855c695c7a860ff49894d4c
lf2007hf <707908214@qq.com>
2024-05-16 13:02:50 +0800

    LGUIComponentReference支持meta设置AllowedClasses

--- Source/LGUIEditor/Private/DetailCustomization/LGUIComponentReferenceCustomization.h
+++ Source/LGUIEditor/Private/DetailCustomization/LGUIComponentReferenceCustomization.h
@@ -19,19 +19,29 @@ public:
 
 	static TSharedRef<IPropertyTypeCustomization> MakeInstance();
 	/** IDetailCustomization interface */
-	virtual void CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) override {};
+	virtual void CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
 	virtual void CustomizeChildren(TSharedRef<IPropertyHandle> PropertyHandle, IDetailChildrenBuilder& ChildBuilder, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
 
 private:
+	void BuildClassFilters();
+
 	TSharedPtr<IPropertyUtilities> PropertyUtilites;
 	TSharedRef<SWidget> OnGetMenu(TSharedPtr<IPropertyHandle> CompProperty, TSharedPtr<IPropertyHandle> CompNameProperty, TArray<UActorComponent*> Components);
 	void OnSelectComponent(TSharedPtr<IPropertyHandle> CompProperty, TSharedPtr<IPropertyHandle> CompNameProperty, UActorComponent* Comp);
 	FText GetButtonText(TSharedPtr<IPropertyHandle> TargetCompHandle, TArray<UActorComponent*> Components)const;
-	void OnCopy(AActor* TargetActor, UActorComponent* TargetComp, UClass* TargetClass);
-	void OnPaste(TSharedPtr<IPropertyHandle> TargetActorProperty, TSharedPtr<IPropertyHandle> TargetCompProperty, TSharedPtr<IPropertyHandle> TargetClassProperty);
-	void OnHelperActorValueChange(TSharedPtr<IPropertyHandle> HelperActorHandle, TSharedPtr<IPropertyHandle> HelperCompHandle, TSharedPtr<IPropertyHandle> HelperClassHandle, TSharedPtr<IPropertyHandle> HelperComponentNameHandle);
-	FReply OnClickFixComponentReference(TSharedPtr<IPropertyHandle> HelperCompHandle, UActorComponent* Target);
+	void OnCopy();
+	void OnPaste();
+	void OnHelperActorValueChange();
+	void RegenerateContentWidget();
+	void OnResetToDefaultClicked();
 
+	TSharedPtr<IPropertyHandle> PropertyHandle;
+	/** Classes that can be used with this property */
+	TArray<const UClass*> AllowedComponentClassFilters;
+	/** Classes that can NOT be used with this property */
+	TArray<const UClass*> DisallowedComponentClassFilters;
+	TSharedPtr<SBox> ContentWidgetBox;
+	bool bIsInWorld = false;
 	TArray<FLGUIComponentReference*> ComponentReferenceInstances;
 	static TWeakObjectPtr<AActor> CopiedHelperActor;
 	static TWeakObjectPtr<UActorComponent> CopiedTargetComp;
