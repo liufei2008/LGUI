cb9f970e176b7ded756d73d30b8b23afb1eaa09a
lf2007hf <707908214@qq.com>
2024-06-06 11:43:05 +0800

    修复：接口设置ClipRectOffset无效

--- Source/LGUI/Private/Core/ActorComponent/LGUICanvas.cpp
+++ Source/LGUI/Private/Core/ActorComponent/LGUICanvas.cpp
@@ -2463,6 +2463,7 @@ void ULGUICanvas::SetRectClipOffset(FMargin newOffset)
 	if (clipRectOffset != newOffset)
 	{
 		MarkRectClipParameterChanged_Recursive();
+		clipRectOffset = newOffset;
 		MarkCanvasUpdate(true, true, false);
 	}
 }
