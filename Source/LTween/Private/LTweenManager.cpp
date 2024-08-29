b71c0040476fdd3b62f04ad4a92276e593119bfe
lf2007hf <707908214@qq.com>
2024-08-29 13:24:58 +0800

    修复：LTween上一次升级之后，GamePause的支持失效了

--- Source/LTween/Private/LTweenManager.cpp
+++ Source/LTween/Private/LTweenManager.cpp
@@ -32,6 +32,7 @@ ULTweenTickHelperComponent::ULTweenTickHelperComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 	PrimaryComponentTick.bStartWithTickEnabled = true;
 	PrimaryComponentTick.TickGroup = ETickingGroup::TG_DuringPhysics;
+	PrimaryComponentTick.bTickEvenWhenPaused = true;
 }
 void ULTweenTickHelperComponent::BeginPlay()
 {
@@ -55,7 +56,7 @@ ALTweenTickHelperActor::ALTweenTickHelperActor()
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
 	PrimaryActorTick.TickGroup = ETickingGroup::TG_DuringPhysics;
-	PrimaryActorTick.bTickEvenWhenPaused = false;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
 
 	SpawnCollisionHandlingMethod = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 }
